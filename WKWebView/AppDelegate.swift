56cce190e9a5c13daad93edec67ae93a6d94df99
Albert Sanchez <albert.san.iza@gmail.com>
2017-09-11 08:56:02 -0700

    removed empty spaces

--- WKWebView/AppDelegate.swift
+++ WKWebView/AppDelegate.swift
@@ -5,22 +5,12 @@
 //  Created by Albert Sanchez on 9/11/17.
 //  Copyright © 2017 Albert Sanchez. All rights reserved.
 //
-
 import Cocoa
-
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
     }
-
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
-
 }
 
--- WKWebView/ViewController.swift
+++ WKWebView/ViewController.swift
@@ -5,23 +5,14 @@
 //  Created by Albert Sanchez on 9/11/17.
 //  Copyright © 2017 Albert Sanchez. All rights reserved.
 //
-
 import Cocoa
-
 class ViewController: NSViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
         }
     }
-
-
 }
 
