c2349b45539524cf8c71c07ba412ba43f63a532a
Albert Sanchez <albert.san.iza@gmail.com>
2017-09-11 08:53:35 -0700

    Initial commit

---
+++ WKWebView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WKWebView
+//
+//  Created by Albert Sanchez on 9/11/17.
+//  Copyright © 2017 Albert Sanchez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
