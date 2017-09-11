51a85729343e757fda2eb0e4d772c7c58c31d06c
Albert Sanchez <albert.san.iza@gmail.com>
2017-09-11 09:15:47 -0700

    added buttons some functionality

--- WKWebView/ViewController.swift
+++ WKWebView/ViewController.swift
@@ -17,6 +17,20 @@ class ViewController: NSViewController {
         didSet {
         }
     }
+    @IBAction func appleClicked(_ sender: Any) {
+        webView.load(myRequest(url: "https://www.apple.com"))
+    }
+    @IBAction func googleClicked(_ sender: Any) {
+        webView.load(myRequest(url: "https://www.google.com"))
+    }
+    @IBAction func localFilesClicked(_ sender: Any) {
+        do {
+            let filePath = Bundle.main.path(forResource: "index", ofType: "html")
+            let contents =  try String(contentsOfFile: filePath!, encoding: .utf8)
+            let baseUrl = URL(fileURLWithPath: filePath!)
+            webView.loadHTMLString(contents as String, baseURL: baseUrl)
+        } catch { }
+    }
     func myRequest(url: String) -> URLRequest {
         return URLRequest(url: URL(string: url)!)
     }
